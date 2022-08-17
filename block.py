493f2f04614b69d1192362d9c751e0403c16a6cb
DamianBisewski <92164738+DamianBisewski@users.noreply.github.com>
2022-08-17 16:12:41 +0200

    Add files via upload

--- block.py
+++ block.py
@@ -1,11 +1,36 @@
-from Crypto.Cipher import AES
-key = "aaaabbbbccccdddd"
-key2 = bytes(key, "utf-8")
-cipher = AES.new(key2, AES.MODE_ECB)
-with open("hand.bmp", "rb") as f:
+
+with open("key.txt") as keyfile:
+    key = keyfile.read()
+iv = "0000111122223333"
+with open("plain.bmp", "rb") as f:
     clear = f.read()
-clear_trimmed = clear[128:-8]
-ciphertext = cipher.encrypt(clear_trimmed)
-ciphertext = clear[0:128] + ciphertext + clear[-8:]
-with open("hand_ecb.bmp", "wb") as f:
+header, body = clear[:54], clear[54:]
+length = len(key) - 1
+print(length)
+ciphertext = bytes()
+for i in range(0, len(body), length):
+    block = bytes(body[i:i + length])
+    encoded = bytes()
+    for j in range(0, 16):
+        xored = block[j] ^ ord(key[j])
+        xored2 = xored.to_bytes(1, "big")
+        encoded = encoded + xored2
+    ciphertext = ciphertext + encoded
+ciphertext = header + ciphertext
+with open("ecb_crypto.bmp", "wb") as f:
     f.write(ciphertext)
+iv = bytes(iv, "utf-8")
+ciphertext2 = bytes()
+for i in range(0, len(body), length):
+    block = body[i:i + length]
+    encoded = bytes()
+    for j in range(0, 16):
+        xored = block[j] ^ iv[j]
+        xored2 = xored ^ ord(key[j])
+        xored3 = xored2.to_bytes(1, "big")
+        encoded = encoded + xored3
+    ciphertext2 = ciphertext2 + encoded
+    iv = encoded
+ciphertext2 = header + ciphertext2
+with open("cbc_crypto.bmp", "wb") as f:
+    f.write(ciphertext2)
